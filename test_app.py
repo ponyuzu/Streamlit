df2423dcc99da655a5d1c11c348f4f182e7c5f01
yuzu <yuzukaw@yahoo.co.jp>
2025-02-24 23:53:26 +0900

    create basic UI

--- test_app.py
+++ test_app.py
@@ -1,4 +1,13 @@
 import streamlit as st
 
-st.title("Hello, Streamlit!")
-st.write("これはテストアプリです。")
+st.title("Hello World")
+st.write("これは初アプリです")
+
+name = st.text_input("あなたの名前を教えてください")
+st.write("あなたの名前",name)
+
+if st.button("クリック！"):
+    st.write("クリックされました！")
+
+age = st.slider("あなたの年齢は?",0,100,20)
+st.write("あなたの年齢は",age,"歳")
